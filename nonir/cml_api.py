6ba0153cf3ce30267cb83939f0b32a1f34d7ebe2
Michael <knappmi14@gmail.com>
2022-02-15 14:20:04 -0500

    more functionality

--- nonir/cml_api.py
+++ nonir/cml_api.py
@@ -2,7 +2,7 @@ import requests
 import urllib3
 import json
 
-def getLab(node):
+def getNodeConfig(node: str) -> str:
     labId = '0dd1ab'
     url = f'https://192.168.128.20/api/v0/labs/{labId}/nodes/{node}'
     headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb20uY2lzY28udmlybCIsImlhdCI6MTY0NDk0MTE5MiwiZXhwIjoxNjQ1MDI3NTkyLCJzdWIiOiJkMGNhMTM4Ni0xMDViLTQzMTctYmNiZi03Nzc0ZDE2NTQwZmUifQ.TnjzkAXmmOn2XY3u_BTTWY2BzwtRHh3BR5Q_ktUlV68'}
@@ -12,6 +12,7 @@ def getLab(node):
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    getLab('n1')
+    getNodeConfig('n1')
+    getNodeConfig('n2')
 
 main()
