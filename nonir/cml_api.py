1f25f3666387ce9d0e5e57640e31bb5fb8cf23fb
Michael <knappmi14@gmail.com>
2022-02-15 12:31:06 -0500

    cml_api

--- nonir/cml_api.py
+++ nonir/cml_api.py
@@ -1,16 +1,17 @@
 import requests
 import urllib3
+import json
 
 def getLab(node):
-    labId = 'ebbd8e'
-    url = f'https://192.168.68.132/api/v0/labs/{labId}/nodes/{node}'
-    headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb20uY2lzY28udmlybCIsImlhdCI6MTY0NDc4NTg0MCwiZXhwIjoxNjQ0ODcyMjQwLCJzdWIiOiJiMDc3ZDY4Mi05ZjU4LTQ0MjAtYTI5ZS03MjdmOGVmMGI3MmYifQ.d-zaJ5ar-eAkEgKKEqWg7VOYO4GRWb0DKGP_pgQAseI'}
+    labId = '0dd1ab'
+    url = f'https://192.168.128.20/api/v0/labs/{labId}/nodes/{node}'
+    headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb20uY2lzY28udmlybCIsImlhdCI6MTY0NDk0MTE5MiwiZXhwIjoxNjQ1MDI3NTkyLCJzdWIiOiJkMGNhMTM4Ni0xMDViLTQzMTctYmNiZi03Nzc0ZDE2NTQwZmUifQ.TnjzkAXmmOn2XY3u_BTTWY2BzwtRHh3BR5Q_ktUlV68'}
     output = requests.get(url, headers = headers, verify = False)
-    print(output.text)
-
+    string = json.loads(output.text)
+    print(string['data']['configuration'])
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    print(getLab('n1'))
+    getLab('n1')
 
 main()
